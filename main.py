c2410e46b1738658301874817b4a7efabe7cc260
ramgiteven <ramsteven+github@gmail.com>
2024-10-18 20:49:49 -0500

    adding route

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from app import create_app
+import jsonify
 
 app = create_app()
+
+@app.route('/', methods=['GET'])
+def ping():
+    try: 
+        return jsonify({"message": successfull}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
 
 if __name__ == "__main__":
     
